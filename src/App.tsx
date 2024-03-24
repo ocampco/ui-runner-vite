a34e369b1ac300b02f329fb941dd29e90a6f25f8
ocampco <ocampco@gmail.com>
2024-03-24 17:04:20 +1100

    remove default exercist

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 // import FileTree from './exercises/FileTree';
+// import TicTacToe from './exercises/TicTacToe';
 import './App.css';
 
-const App = () => <h1>App.js</h1>;
+const App = () => <h1>App.tsx</h1>;
 
 export default App;
